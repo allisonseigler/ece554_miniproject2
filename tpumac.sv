d0e78b1035598b9deb0a10b6908c049a905716e9
Allison Seigler <allisonseigler@Allisons-MacBook-Pro.local>
2022-09-15 13:42:01 -0500

    added verilog files

---
+++ tpumac.sv
@@ -0,0 +1,16 @@
+// Spec v1.1
+module tpumac
+ #(parameter BITS_AB=8,
+   parameter BITS_C=16)
+  (
+   input clk, rst_n, WrEn, en,
+   input signed [BITS_AB-1:0] Ain,
+   input signed [BITS_AB-1:0] Bin,
+   input signed [BITS_C-1:0] Cin,
+   output reg signed [BITS_AB-1:0] Aout,
+   output reg signed [BITS_AB-1:0] Bout,
+   output reg signed [BITS_C-1:0] Cout
+  );
+// NOTE: added register enable in v1.1
+// Also, Modelsim prefers "reg signed" over "signed reg"
+
